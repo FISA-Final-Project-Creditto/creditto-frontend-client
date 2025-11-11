630d4a41b0b95f93e8aee1e5604886b730058d8b
wjddydwns <wjddydwns159@gmail.com>
2025-11-11 11:20:10 +0900

    feat :  회원가입 UI 완료

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,42 +1,34 @@
-'use client'
+"use client";
 import Image from "next/image";
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export default function SplashPage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    // 1) 단순 딜레이 후 이동
-    const t = setTimeout(() => {
-      router.replace('/home')   // 첫 화면 경로
-    }, 1200)
-
-    // 2) 예: 토큰 체크/초기 로딩 후 이동도 가능
-    // async function init() {
-    //   await hydrateUser() // 예: 로컬스토리지/IndexedDB/RTK Query 프리패치
-    //   router.replace('/home')
-    // }
-    // init()
-
-    return () => clearTimeout(t)
-  }, [router])
+  const router = useRouter();
 
   return (
-
-
-   
-    <main className="grid place-items-center min-h-[100dvh] bg-white text-black overflow-hidden">
-      <div className="relative w-[min(92vw,520px)] h-[60vh]">
+ <main className="h-[100dvh] flex justify-center bg-[#e5e5e5]">
+      <div className="w-full max-w-[440px] min-h-[100dvh] mx-auto flex flex-col justify-end bg-white ">
         <Image
           src="/접속로딩1.svg"
           alt="logo"
           fill
-          className="object-contain"
-          priority
+          className="object-fill "
         />
+
+        <div className="relative z-10">
+          <div className="w-full h-[73px] cursor-pointer flex justify-center items-center text-gray-300 underline text-lg font-medium"
+          onClick={(e)=>{
+            e.stopPropagation();
+          router.push('/signup/permission')}}>
+            회원가입
+          </div>
+          <button
+            className="w-full h-[73px] bg-[#1A3668] text-white text-[26px] font-semibold flex justify-center items-center"
+            onClick={() => router.push("/auth/pw")}>
+            로그인
+          </button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
-
--- src/app/signup/success/page.jsx
+++ src/app/signup/success/page.jsx
@@ -3,37 +3,35 @@ import React from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { useRouter } from "next/navigation";
 
-export default function LoadingPage() {
+export default function SuccessPage() {
   const router = useRouter();
-
-  const autoPage = () => {
-    setTimeout(() => {
-      router.push("/signup/success");
-    }, 5000);
-  };
-
-  autoPage();
-
   return (
-    <main className="h-[100dvh] flex justify-center items-center bg-[#e5e5e5]">
+    <main className="h-[100dvh] flex justify-end items-center bg-[#e5e5e5]">
       <div className="w-full max-w-[440px] min-h-[100dvh] mx-auto flex flex-col bg-white">
         <h1 className="text-[22px] font-semibold text-gray-900 mt-17"></h1>
         <div className="flex-1 px-8 mt-5 ">
           <p className="text-2xl font-semibold leading-relaxed text-gray-900 text-left">
-            고객님의 정보를 확인하고 있어요
+            가입이 완료되었어요
             <br />
-            <p className="text-lg font-normal text-gray-500 mb-5">
-              잠시만 기다려 주세요
+            <p className="text-lg font-normal text-gray-500 mb-10">
+              creditto의 서비스를 이용하기 위해서 
+              <br/>
+              인증서 발급이 필요해요
             </p>
           </p>
           <div>
             <DotLottieReact
-              src="https://lottie.host/5e3d00b3-79ca-4abd-9808-99013190e330/8EXayNOkme.lottie"
+              src="https://lottie.host/5c64549d-d983-4d34-bc50-680299f731ce/EO1GXl7zwp.lottie"
               loop
               autoplay
             />
           </div>
         </div>
+               <button
+            className="w-full h-[73px] bg-[#1A3668] text-white text-[26px] font-semibold flex justify-center items-center"
+            onClick={() => router.push("/auth/pw")}>
+            인증서 발급하기
+          </button>
       </div>
     </main>
   );
