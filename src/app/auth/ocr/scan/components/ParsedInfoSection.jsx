20876b12db249374943ae655f7a24f033e677831
TaekkiMin <tktwin00@gmail.com>
2025-11-19 10:44:55 +0900

    feat: OCR 기능 구현 및 디자인
    - NAVER CLOVA API 기능을 활용해서 외국인등록증 분석 기능 구현

---
+++ src/app/auth/ocr/scan/components/ParsedInfoSection.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import BottomBar from "../../components/BottomBar";
+import InfoDisplayField from "./InfoDisplayField";
+
+export default function ParsedInfoSection({ parsedData }) {
+  return (
+    <div className="flex flex-col text-left">
+      <h1 className="text-2xl font-bold mb-[20px]">아래 정보가 맞나요?</h1>
+
+      <div className="flex flex-col items-start space-y-[0.938rem]">
+        <InfoDisplayField
+          label="외국인등록번호"
+          value={parsedData?.alienRegNum ?? ""}
+        />
+        <InfoDisplayField label="이름" value={parsedData?.name ?? ""} />
+        <InfoDisplayField label="성별" value={parsedData?.sex ?? ""} />
+        <InfoDisplayField
+          label="국가 / 지역"
+          value={parsedData?.nationality ?? ""}
+        />
+        <InfoDisplayField
+          label="발급 일자"
+          value={parsedData?.issueDateIso || parsedData?.issueDateRaw || "-"}
+        />
+        <InfoDisplayField
+          label="비자 유형"
+          value={parsedData?.visaType ?? ""}
+        />
+        <InfoDisplayField
+          label="발급 기관"
+          value={parsedData?.authority ?? ""}
+        />
+      </div>
+
+      <footer>
+        <BottomBar
+          label="확인"
+          onClick={() => router.push("/auth/info")}
+          isActive={true}
+        />
+      </footer>
+    </div>
+  );
+}
