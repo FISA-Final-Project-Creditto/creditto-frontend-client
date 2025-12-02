653f490b9b4bcec6da87c3cca0b652b02f965e60
TaekkiMin <tktwin00@gmail.com>
2025-12-02 09:41:36 +0900

    chore : next.config.js에서 API rewrite 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,18 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-import NextPWA from 'next-pwa';
+import createNextIntlPlugin from "next-intl/plugin";
+import NextPWA from "next-pwa";
 
 const withNextIntl = createNextIntlPlugin();
 const withPWA = NextPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development' || process.env.DISABLE_PWA === 'true',
+  dest: "public",
+  disable:
+    process.env.NODE_ENV === "development" ||
+    process.env.DISABLE_PWA === "true",
   register: true,
   skipWaiting: true,
 });
 
 const nextConfig = withNextIntl({
   // 기존 설정들...
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
-      },
-    ];
-  },
 });
 
 export default withPWA(nextConfig);
