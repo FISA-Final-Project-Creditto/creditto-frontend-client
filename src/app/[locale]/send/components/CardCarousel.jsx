48f298d631fedf69f8dd86cbc7a41e527ceec77a
TaekkiMin <tktwin00@gmail.com>
2025-12-03 04:56:01 +0900

    refactor : 다른 팀원의 작업 예정으로 주석 처리

--- src/app/[locale]/send/components/CardCarousel.jsx
+++ src/app/[locale]/send/components/CardCarousel.jsx
@@ -35,34 +35,35 @@ export default function CardCarousel() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
 
-  useEffect(() => {
-    // 계좌 잔액 합산 조회 by UserId
-    const fetchAccountBalance = async () => {
-      try {
-        const accessToken = sessionStorage.getItem("accessToken");
+  // 다른 팀원의 작업으로 주석 처리!!
+  // useEffect(() => {
+  //   // 계좌 잔액 합산 조회 by UserId
+  //   const fetchAccountBalance = async () => {
+  //     try {
+  //       const accessToken = sessionStorage.getItem("accessToken");
 
-        const res = await credittoApi.get("/api/accounts/me/balance", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
+  //       const res = await credittoApi.get("/api/accounts/me/balance", {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //         },
+  //       });
 
-        const { code, data } = res.data;
-        if (code === 200) {
-          // 송금 화면 이용 가능
-          console.log("연동된 계좌 있음");
-        } else {
-          // 메인페이지로 이동
-          alert("연동된 계좌가 없습니다");
-          router.replace("/");
-        }
-      } catch (error) {
-        console.error("계좌 잔액 합산 조회 by UserId 오류 발생: ", error);
-      }
-    };
+  //       const { code, data } = res.data;
+  //       if (code === 200) {
+  //         // 송금 화면 이용 가능
+  //         console.log("연동된 계좌 있음");
+  //       } else {
+  //         // 메인페이지로 이동
+  //         alert("연동된 계좌가 없습니다");
+  //         router.replace("/");
+  //       }
+  //     } catch (error) {
+  //       console.error("계좌 잔액 합산 조회 by UserId 오류 발생: ", error);
+  //     }
+  //   };
 
-    fetchAccountBalance();
-  }, []);
+  //   fetchAccountBalance();
+  // }, []);
 
   return (
     <div className="flex-1 flex justify-center relative">
--- src/app/[locale]/send/consent/page.jsx
+++ src/app/[locale]/send/consent/page.jsx
@@ -18,43 +18,40 @@ export default function ConsentPage() {
   }); // 연동 계좌
 
   useEffect(() => {
+    // 다른 팀원의 구현으로 주석 처리!!
     // 계좌 잔액 합산 조회 by UserId
-    const fetchAccountBalance = async () => {
-      try {
-        const accessToken = sessionStorage.getItem("accessToken");
-
-        const res = await credittoApi.get("/api/accounts/me/balance", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const { code, data } = res.data;
-        if (code === 200) {
-          setAccountState(data); // 응답값을 setAccountState에 상태 저장
-          console.log("연동된 계좌 있음", data);
-
-          return true;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.error("계좌 잔액 합산 조회 by UserId 오류 발생: ", error);
-      }
-    };
-
+    // const fetchAccountBalance = async () => {
+    //   try {
+    //     const accessToken = sessionStorage.getItem("accessToken");
+    //     const res = await credittoApi.get("/api/accounts/me/balance", {
+    //       headers: {
+    //         Authorization: `Bearer ${accessToken}`,
+    //       },
+    //     });
+    //     const { code, data } = res.data;
+    //     if (code === 200) {
+    //       setAccountState(data); // 응답값을 setAccountState에 상태 저장
+    //       console.log("연동된 계좌 있음", data);
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   } catch (error) {
+    //     console.error("계좌 잔액 합산 조회 by UserId 오류 발생: ", error);
+    //   }
+    // };
     // 연동된 계좌가 있으면 송금 방식 저장
-    if (fetchAccountBalance()) {
-      const type = searchParams.get("type");
-      // URL에 type 파라미터가 있을 경우에만 Redux 상태를 업데이트
-      if (type) {
-        dispatch(setModeData(type)); // 송금 방식 저장
-      }
-      // 없으면 메인페이지로 이동
-    } else {
-      alert("연동된 계좌가 없습니다.");
-      router.replace("/");
-    }
+    // if (fetchAccountBalance()) {
+    //   const type = searchParams.get("type");
+    //   // URL에 type 파라미터가 있을 경우에만 Redux 상태를 업데이트
+    //   if (type) {
+    //     dispatch(setModeData(type)); // 송금 방식 저장
+    //   }
+    //   // 없으면 메인페이지로 이동
+    // } else {
+    //   alert("연동된 계좌가 없습니다.");
+    //   router.replace("/");
+    // }
   }, []); // searchParams가 변경될 때마다 이 효과를 다시 실행
 
   // 약관 동의서
