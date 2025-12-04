99741ea9ed99eefddabbb46f097b0892432129a6
TaekkiMin <tktwin00@gmail.com>
2025-12-05 04:55:32 +0900

    refactor: /send 페이지 뒤로가기를 무조건 /main 페이지로 이동하게도록 구현

--- src/app/[locale]/send/page.jsx
+++ src/app/[locale]/send/page.jsx
@@ -3,7 +3,7 @@
 import CardCarousel from "./components/CardCarousel";
 import AppHeader from "@/src/common/AppHeader/AppHeader";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useTranslations } from "next-intl";
 import { useDispatch } from "react-redux";
 import { setConsentChecked } from "@/src/store/features/consent/consentSlice";
@@ -12,6 +12,7 @@ import { clearModeData } from "@/src/store/features/send/sendModeSlice";
 export default function SendMainPage() {
   const t = useTranslations("send.common");
   const dispatch = useDispatch();
+  const router = useRouter();
 
   useEffect(() => {
     // 약관 ID 리스트 아이디 초기화
@@ -39,6 +40,7 @@ export default function SendMainPage() {
         show={true}
         showHamburger={false}
         showBack={true}
+        onBackClick={() => router.replace("/main")} // 메인페이지로 이동하는 건 replace로
       />
 
       <div className="flex-1 flex flex-col px-5 relative">
