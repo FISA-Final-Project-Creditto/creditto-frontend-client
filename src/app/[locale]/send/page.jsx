e63dbb5a44f49ef4a468e61014e674944f4a1d0e
TaekkiMin <tktwin00@gmail.com>
2025-12-07 13:37:52 +0900

    perf: TTFB 감소를 위해 정적 코드 추가

--- src/app/[locale]/send/page.jsx
+++ src/app/[locale]/send/page.jsx
@@ -5,6 +5,8 @@ export const metadata = {
   description: "정기 및 일회성 해외 송금 기능",
 };
 
+export const dynamic = "force-static"; // 정적으로 만들어줘서 TTFB 크게 감소시킴
+
 export default function Page() {
   return <SendMainClient />;
 }
